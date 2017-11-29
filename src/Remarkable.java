e6fc7c7984157686e29678f515ecbc9b7cecea78
kiernan.victor <kiernan.victor@SEMS-259-12.SEMS259.LAB>
2017-11-29 14:48:01 -0800

    Unbirthday, remarkable

---
+++ src/Remarkable.java
@@ -0,0 +1,53 @@
+import javax.swing.JOptionPane;
+
+public class Remarkable {
+public static void main(String[] args) {
+	
+	
+String name=JOptionPane.showInputDialog("What is your name");	
+if(name.equalsIgnoreCase("Victor")) {
+	JOptionPane.showMessageDialog(null, "Hello Victor. You are the victor of anything you try at!");
+}
+else if(name.equalsIgnoreCase("Yasir")) {
+	JOptionPane.showMessageDialog(null, "Hi Yasir! You are a very kind and humble person");
+}
+else if(name.equalsIgnoreCase("Max")){
+	JOptionPane.showMessageDialog(null, "Hi Max! You are the maximum amout of awesomeness possible!");
+}
+else if(name.equalsIgnoreCase("Emil")) {
+	JOptionPane.showMessageDialog(null, "Emil, you are an awesome person to know!");
+}
+else if(name.equalsIgnoreCase("Callum")) {
+	JOptionPane.showMessageDialog(null,"Callum, you are an awesome persom" );
+}
+else if(name.equalsIgnoreCase("Lauren")) {
+	JOptionPane.showMessageDialog(null, "Lauren, you are a very smart!");
+}
+else if(name.equalsIgnoreCase("Cruz")) {
+	JOptionPane.showMessageDialog(null, "Hello Cruz. You are a very funny guy");
+}
+else {
+	JOptionPane.showMessageDialog(null,  "USER: "+name+" NOT IN DATABASE! YOU ARE NOT SUPPOSED TO BE HERE SELF DESTRUCT BEGUN! THIS COMPUTER WILL BLOW UP IN 2453 MONTHS!");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+}
---
+++ src/unbirthday.java
@@ -0,0 +1,14 @@
+import javax.swing.JOptionPane;
+
+public class unbirthday {
+public static void main(String[] args) {
+
+	String birthday=JOptionPane.showInputDialog("What is your birthday");
+	if(birthday.equals("11/29")) {
+		JOptionPane.showMessageDialog(null, "Happy Birthday!");
+	}
+	else {
+		JOptionPane.showMessageDialog(null, "Merry Unbirthday!");
+	}
+}
+}
